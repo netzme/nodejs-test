77d5b5c76a6b3185f1c11a98329a8d4611a89b2e
Untung Ginardi <untung.ginardi@gmail.com>
2015-01-19 05:50:08 +0700

    menambahkan Gruntfile.js untuk penggunaan build tool grunt

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+/**
+ * Created by untung on 1/19/15.
+ */
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> */\n'
+            },
+            build: {
+                files: [
+                    {
+                        src: ['server.js', '!Gruntfile.js'],
+                        dest: 'build/server.min.js'
+                    },
+                    {
+                        src: ['mosca.js', '!Gruntfile.js'],
+                        dest: 'build/mosca.min.js'
+                    }
+                ]
+            }
+        },
+        clean: {
+            build: ['build/*.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('default', ['uglify']);
+}
